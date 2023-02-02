293c1698a1b78ce44cec0754fe1eb85b87c25570
Pablo Torregrosa Paez <ppaez@life360.com>
2023-02-02 22:58:52 +0100

    refactor: Split adding redis entries from exposing the hosts

--- web_server/server.rb
+++ web_server/server.rb
@@ -3,18 +3,19 @@ require "sinatra/json"
 require "redis"
 
 ### Redis client
-
 redis = Redis.new(:host => "redis")
+def add_entry_to_redis(redis)
+  key = "#{rand(20000)}-foo" # Introduce new items on every endpoint call
+  redis.set(key, "bar");
+  p redis.get(key);
+end
 
 ### Web server ###
 set :bind, '0.0.0.0' # To make the endpoint available outside localhost
 set :port, 80
 
 get '/targets' do
-  key = "#{rand(20000)}-foo" # Introduce new items on every endpoint call
-  redis.set(key, "bar");
-  p redis.get(key);
-
+  add_entry_to_redis(redis)
   json [
     { "targets": ["redis:6379"] }
   ]
